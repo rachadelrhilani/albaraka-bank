52bc48e69f512e619609b2436ffd57b53fdb49c3
rachadelrhilani <rachdos76@gmail.com>
2026-09-24 22:37:47 +0100

    creer les exceptions

---
+++ exception/MontantInvalideException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class MontantInvalideException extends Exception {
+    public MontantInvalideException(String message) {
+        super(message);
+    }
+}
---
+++ exception/SoldeInsuffisantException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class SoldeInsuffisantException extends Exception {
+    public SoldeInsuffisantException(String message) {
+        super(message);
+    }
+}
