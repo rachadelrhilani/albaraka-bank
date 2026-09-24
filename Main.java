ad6aab965c68e0dd139829051dc9268637f6f8d6
rachadelrhilani <rachdos76@gmail.com>
2026-09-24 22:51:32 +0100

    creer Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
-class Main {
-    public static void main(String[] args){
-        System.out.println("hello");
+import dao.ClientDAO;
+import dao.CompteDAO;
+import dao.TransactionDAO;
+import service.ClientService;
+import service.CompteService;
+import service.RapportService;
+import service.TransactionService;
+import ui.MainMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialisation des DAOs
+        ClientDAO clientDAO = new ClientDAO();
+        CompteDAO compteDAO = new CompteDAO();
+        TransactionDAO transactionDAO = new TransactionDAO();
+
+        // Initialisation des Services
+        ClientService clientService = new ClientService(clientDAO, compteDAO);
+        CompteService compteService = new CompteService(compteDAO, clientDAO);
+        TransactionService transactionService = new TransactionService(transactionDAO, compteDAO);
+        RapportService rapportService = new RapportService(clientDAO, compteDAO, transactionDAO);
+
+        // Lancement de l'UI
+        MainMenu menu = new MainMenu(clientService, compteService, transactionService, rapportService);
+        menu.demarrer();
     }
 }
